524e937c0a46dd146352f8cf7564ebbb6bddc8c4
Savio Moraes <smmsavio@hotmail.com>
2021-12-21 14:05:21 -0300

    Updates

---
+++ precache-manifest.5d1eaa73b6f7d8bfea943fbe415bcae3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fc6f6ffa9c6e384d37f6d9cee3cbd0a",
+    "url": "/project-movie-cards-library/index.html"
+  },
+  {
+    "revision": "3a815c4b8a9de4e46e04",
+    "url": "/project-movie-cards-library/static/css/main.cf8f3cea.chunk.css"
+  },
+  {
+    "revision": "0d4f78d6a282aeb41de1",
+    "url": "/project-movie-cards-library/static/js/2.9a6a2542.chunk.js"
+  },
+  {
+    "revision": "3a815c4b8a9de4e46e04",
+    "url": "/project-movie-cards-library/static/js/main.21869e59.chunk.js"
+  },
+  {
+    "revision": "b5cd559347f125a80e22",
+    "url": "/project-movie-cards-library/static/js/runtime-main.136c6182.js"
+  }
+]);
